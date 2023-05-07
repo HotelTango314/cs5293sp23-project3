4eb8de0ee6aeedd2620e284425cda1ed7577f837
HotelTango314 <henry.r.thomas-1@ou.edu>
2023-05-07 05:01:29 +0000

    eod 5/6/2023

---
+++ project3.py
@@ -0,0 +1,290 @@
+import numpy as np
+from pypdf import PdfReader
+import os
+import pathlib
+import argparse
+import pandas as pd
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import words
+import spacy
+import en_core_web_md
+import unicodedata
+#from contractions import CONTRACTION_MAP
+import re
+from nltk.corpus import wordnet
+import collections
+#from textblob import Word
+from nltk.tokenize.toktok import ToktokTokenizer
+from bs4 import BeautifulSoup
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction import CountVectorizer
+import pickle
+
+
+CONTRACTION_MAP = {
+        "ain't": "is not",
+        "aren't": "are not",
+        "can't": "cannot",
+        "can't've": "cannot have",
+        "'cause": "because",
+        "could've": "could have",
+        "couldn't": "could not",
+        "couldn't've": "could not have",
+        "didn't": "did not",
+        "doesn't": "does not",
+        "don't": "do not",
+        "hadn't": "had not",
+        "hadn't've": "had not have",
+        "hasn't": "has not",
+        "haven't": "have not",
+        "he'd": "he would",
+        "he'd've": "he would have",
+        "he'll": "he will",
+        "he'll've": "he he will have",
+        "he's": "he is",
+        "how'd": "how did",
+        "how'd'y": "how do you",
+        "how'll": "how will",
+        "how's": "how is",
+        "I'd": "I would",
+        "I'd've": "I would have",
+        "I'll": "I will",
+        "I'll've": "I will have",
+        "I'm": "I am",
+        "I've": "I have",
+        "i'd": "i would",
+        "i'd've": "i would have",
+        "i'll": "i will",
+        "i'll've": "i will have",
+        "i'm": "i am",
+        "i've": "i have",
+        "isn't": "is not",
+        "it'd": "it would",
+        "it'd've": "it would have",
+        "it'll": "it will",
+        "it'll've": "it will have",
+        "it's": "it is",
+        "let's": "let us",
+        "ma'am": "madam",
+        "mayn't": "may not",
+        "might've": "might have",
+        "mightn't": "might not",
+        "mightn't've": "might not have",
+        "must've": "must have",
+        "mustn't": "must not",
+        "mustn't've": "must not have",
+        "needn't": "need not",
+        "needn't've": "need not have",
+        "o'clock": "of the clock",
+        "oughtn't": "ought not",
+        "oughtn't've": "ought not have",
+        "shan't": "shall not",
+        "sha'n't": "shall not",
+        "shan't've": "shall not have",
+        "she'd": "she would",
+        "she'd've": "she would have",
+        "she'll": "she will",
+        "she'll've": "she will have",
+        "she's": "she is",
+        "should've": "should have",
+        "shouldn't": "should not",
+        "shouldn't've": "should not have",
+        "so've": "so have",
+        "so's": "so as",
+        "that'd": "that would",
+        "that'd've": "that would have",
+        "that's": "that is",
+        "there'd": "there would",
+        "there'd've": "there would have",
+        "there's": "there is",
+        "they'd": "they would",
+        "they'd've": "they would have",
+        "they'll": "they will",
+        "they'll've": "they will have",
+        "they're": "they are",
+        "they've": "they have",
+        "to've": "to have",
+        "wasn't": "was not",
+        "we'd": "we would",
+        "we'd've": "we would have",
+        "we'll": "we will",
+        "we'll've": "we will have",
+        "we're": "we are",
+        "we've": "we have",
+        "weren't": "were not",
+        "what'll": "what will",
+        "what'll've": "what will have",
+        "what're": "what are",
+        "what's": "what is",
+        "what've": "what have",
+        "when's": "when is",
+        "when've": "when have",
+        "where'd": "where did",
+        "where's": "where is",
+        "where've": "where have",
+        "who'll": "who will",
+        "who'll've": "who will have",
+        "who's": "who is",
+        "who've": "who have",
+        "why's": "why is",
+        "why've": "why have",
+        "will've": "will have",
+        "won't": "will not",
+        "won't've": "will not have",
+        "would've": "would have",
+        "wouldn't": "would not",
+        "wouldn't've": "would not have",
+        "y'all": "you all",
+        "y'all'd": "you all would",
+        "y'all'd've": "you all would have",
+        "y'all're": "you all are",
+        "y'all've": "you all have",
+        "you'd": "you would",
+        "you'd've": "you would have",
+        "you'll": "you will",
+        "you'll've": "you will have",
+        "you're": "you are",
+        "you've": "you have"
+}
+
+nlp = en_core_web_md.load()
+tokenizer = ToktokTokenizer()
+
+def process_pdf(filename):
+    with open(filename,'rb') as f:
+        doc = PdfReader(f)
+        text = ''
+        for x in doc.pages:
+            text =text + ' '+x.extract_text()
+    name = filename.split('.')[0]
+    return text, name
+
+def strip_html_tags(text):
+    soup = BeautifulSoup(text, "html.parser")
+    if bool(soup.find()):
+        [s.extract() for s in soup(['iframe', 'script'])]
+        stripped_text = soup.get_text()
+        stripped_text = re.sub(r'[\r|\n|\r\n]+', '\n', stripped_text)
+    else:
+        stripped_text = text
+    return stripped_text
+    #return soup.text
+
+def simple_porter_stemming(text):
+    ps = nltk.porter.PorterStemmer()
+    text = ' '.join([ps.stem(word) for word in text.split()])
+    return text
+
+def lemmatize_text(text):
+    #nlp = en_core_web_md.load()
+    text = nlp(text)
+    text = ' '.join([word.lemma_ if word.lemma_ != '-PRON-' else word.text for word in text])
+    return text
+
+
+def remove_repeated_characters(tokens):
+    repeat_pattern = re.compile(r'(\w*)(\w)\2(\w*)')
+    match_substitution = r'\1\2\3'
+    def replace(old_word):
+        if wordnet.synsets(old_word):
+            return old_word
+        new_word = repeat_pattern.sub(match_substitution, old_word)
+        return replace(new_word) if new_word != old_word else new_word        
+    correct_tokens = [replace(word) for word in tokens]
+    return correct_tokens
+
+
+def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
+    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),
+            flags=re.IGNORECASE|re.DOTALL)
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = contraction_mapping.get(match)\
+            if contraction_mapping.get(match)\
+            else contraction_mapping.get(match.lower())                       
+        expanded_contraction = first_char+expanded_contraction[1:]
+        return expanded_contraction
+    expanded_text = contractions_pattern.sub(expand_match, text)
+    expanded_text = re.sub("'", "", expanded_text)
+    return expanded_text
+
+def remove_accented_chars(text):
+    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    return text
+
+def remove_special_characters(text):
+    pattern = r'[^a-zA-Z0-9\s]|\[|\]'
+    text = re.sub(pattern, '', text)
+    return text
+
+def remove_stopwords(text):
+    stopwords = nltk.corpus.stopwords.words('english')
+    #tokenizer = ToktokTokenizer()
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    filtered_tokens = [token for token in tokens if token not in stopwords]
+    filtered_text = ' '.join(filtered_tokens)    
+    return filtered_text
+
+def remove_nonwords(text):
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    filtered_tokens = [token for token in tokens if token in nlp.vocab]
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+def remove_commonwords(text, words):
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    filtered_tokens = [token for token in tokens if token not in words]
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+def normalize_corpus(doc):
+    #stopword_list = nltk.corpus.stopwords.words('english')
+    #nlp = en_core_web_md.load()
+    doc = strip_html_tags(doc)
+    doc = doc.translate(doc.maketrans("\n\t\r", "   "))
+    doc = remove_accented_chars(doc)
+    doc = expand_contractions(doc)
+    doc = lemmatize_text(doc)
+    special_char_pattern = re.compile(r'([{.(-)!}])')
+    doc = special_char_pattern.sub(" \\1 ", doc)
+    doc = remove_special_characters(doc)
+    doc = re.sub(' +', ' ', doc)
+    doc = doc.lower()
+    doc = remove_stopwords(doc)
+    doc = remove_nonwords(doc)
+    return doc
+
+def predict_text(doc):
+    lsa = make_pipeline(CountVectorizer
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--document', required=True, help="the new PDF to be classified")
+    
+    args = parser.parse_args()
+    
+    raw_text,cityname = process_pdf(args.document)
+    #print(raw_text)
+    clean_text = normalize_corpus(raw_text)
+    #print(clean_text)
+    lsa = load #NEXT STEP: LOAD PICKLE FOR VECTORIZER TO VECTORIZE NEW DATA
+
+
+    #df = pd.DataFrame([cityname, raw_text, clean_text],columns=['city','raw_text','clean_text'])
+    #print(df.head())
+    
+
+    
+
+
+
+
